478647063b1490ff3aa3637096ed431f342e9314
rebeccahendsey <61711413+rebeccahendsey@users.noreply.github.com>
2021-04-21 14:19:22 -0400

    Register/Login

---
+++ app/src/main/java/edu/fordham/cordial3/LoginActivity.java
@@ -0,0 +1,12 @@
+package edu.fordham.cordial3;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity {
+    EditText username;
+    EditText password;
+    Button register;
+    Button login;
+
+}
